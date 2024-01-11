3faad7f6f813befdfc88f5981b5a9660c52d15af
Burak Kebapci <burak.kebapci@neuro-id.com>
2024-01-11 11:14:13 -0500

    v1

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the cors middleware
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const memcached = require('memcached');
+const client = new memcached();
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Serve static files from the root directory
+app.use(express.static('public'));
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,11 +24,20 @@ app.get('/', (req, res) => {
 });
 
 // Handle form submission
-app.post('/submit', (req, res) => {
+app.post('/submit', async (req, res) => {
   console.log('Form data:', req.body);
   // You can process the form data here
   res.send('Form submitted successfully');
 });
+
+// Proxy API requests to https://api.neuro-id.com/v4.1
+app.use(
+  '/v4.1',
+  createProxyMiddleware({
+    target: 'https://api.neuro-id.com',
+    changeOrigin: true,
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
